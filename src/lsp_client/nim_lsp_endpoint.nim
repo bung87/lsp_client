9d818b1da83b38631921c75cd0b217cc45d56c76
bung87 <crc32@qq.com>
2021-07-03 07:24:21 +0800

    o

--- src/lsp_client/nim_lsp_endpoint.nim
+++ src/lsp_client/nim_lsp_endpoint.nim
@@ -1,9 +1,13 @@
 import ./lsp_types
+import asynctools, asyncdispatch, os, strutils, asyncnet
 
-type LspNimEndpoint* =  LspEndpoint
+from osproc import execCmd
 
+type LspNimEndpoint* = ref object of LspEndpoint
+  process: AsyncProcess
 
-proc start*(self:LspNimEndpoint)= discard
-proc stop*(self:LspNimEndpoint)= discard
-proc sendNotification*(self:LspNimEndpoint,noti:string)= discard
-template callMethod*(self:LspNimEndpoint)= discard
+proc start*(self: LspNimEndpoint) =
+  self.process = startProcess(findExe("nimlsp"))
+proc stop*(self: LspNimEndpoint) = discard
+proc sendNotification*(self: LspNimEndpoint, noti: string) = discard
+template callMethod*(self: LspNimEndpoint) = discard
