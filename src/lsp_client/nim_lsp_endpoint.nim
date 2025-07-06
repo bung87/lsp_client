2282780db66ee7c21fcb81f2930ab060dabf37e6
bung87 <crc32@qq.com>
2025-07-06 23:30:59 +0800

    clean echo

--- src/lsp_client/nim_lsp_endpoint.nim
+++ src/lsp_client/nim_lsp_endpoint.nim
@@ -31,23 +31,35 @@ class pub LspNimEndpoint:
     self.err = self.process.stderrStream()
 
   proc startProcess*() {.async.} =
-    let process = await startProcess(findExe("nimlsp"), options = {},
-                                   stdoutHandle = AsyncProcess.Pipe,
-                                   stderrHandle = AsyncProcess.Pipe,
-                                   stdinHandle = AsyncProcess.Pipe)
+    let process = await startProcess(
+      findExe("nimlsp"),
+      options = {},
+      stdoutHandle = AsyncProcess.Pipe,
+      stderrHandle = AsyncProcess.Pipe,
+      stdinHandle = AsyncProcess.Pipe,
+    )
     self.setProcess(process)
-  proc stopProcess*() = discard
-  proc getId*(): int = self.id
-  proc incId*() = inc self.id
+
+  proc stopProcess*() =
+    discard
+
+  proc getId*(): int =
+    self.id
+
+  proc incId*() =
+    inc self.id
+
   proc exitCode*(): int =
     let result = self.process.peekExitCode()
     if result.isOk():
       result.get()
     else:
       -1
+
   proc write*(p: pointer, len: int): Future[int] {.async.} =
     await self.output.write(p, len)
     result = len
+
   proc readError*(): Future[string] {.async.} =
     try:
       let data = await self.err.read()
@@ -56,13 +68,13 @@ class pub LspNimEndpoint:
         result.add(char(b))
     except CatchableError:
       result = ""
+
   proc readMessage*(): Future[string] {.async.} =
     # Note: nimlsp debug build will produce debug info to stdout
     var contentLen = -1
     var headerStarted = false
     while true:
       let ln = await self.input.readLine()
-      debugEcho ln
       if ln.len != 0:
         let sep = ln.find(':')
         if sep == -1:
@@ -75,8 +87,9 @@ class pub LspNimEndpoint:
             raise newException(UnsupportedEncoding, "only utf-8 is supported")
         of "Content-Length":
           if parseInt(ln, contentLen, valueStart) == 0:
-            raise newException(MalformedFrame, "invalid Content-Length: " &
-                                                ln.substr(valueStart))
+            raise newException(
+              MalformedFrame, "invalid Content-Length: " & ln.substr(valueStart)
+            )
         else:
           # Unrecognized headers are ignored
           continue
@@ -96,43 +109,41 @@ class pub LspNimEndpoint:
         else:
           raise newException(MalformedFrame, "missing Content-Length header")
 
-  proc send*(str: string): Future[void]{.async.} =
+  proc send*(str: string): Future[void] {.async.} =
     var msg: string = "Content-Type: application/vscode-jsonrpc; charset=utf-8\r\n"
     msg = msg & "Content-Length: " & $str.len & "\r\n\r\n" & str
-    debugEcho repr msg
 
     await self.output.write(msg)
     await self.output.finish()
 
-  proc roundtrip*(str: string): Future[string]{.async.} =
+  proc roundtrip*(str: string): Future[string] {.async.} =
     var msg: string = "Content-Type: application/vscode-jsonrpc; charset=utf-8\r\n"
     msg = msg & "Content-Length: " & $str.len & "\r\n\r\n" & str
-    debugEcho repr msg
 
     await self.output.write(msg)
     await self.output.finish()
     result = await self.readMessage()
 
-  proc sendNotification*(`method`: string): Future[void]{.async.} =
+  proc sendNotification*(`method`: string): Future[void] {.async.} =
     let jo = initGRequest(`method` = `method`)
-    let str = $ % jo
+    let str = $ %jo
     await self.send(str)
 
-  proc sendNotification*[T](`method`: string, params: T): Future[void]{.async.} =
+  proc sendNotification*[T](`method`: string, params: T): Future[void] {.async.} =
     let jo = initGRequest(`method` = `method`, params = cast[JsonNode](params))
-    let str = $ % jo
+    let str = $ %jo
     await self.send(str)
 
-  proc callMethod*(`method`: string): Future[string]{.async.} =
+  proc callMethod*(`method`: string): Future[string] {.async.} =
     let id = self.getId()
     self.incId()
     let jo = initGRequest(id = id, `method` = `method`)
-    let str = $ % jo
+    let str = $ %jo
     result = await self.roundtrip(str)
 
-  proc callMethod*[T](`method`: string, params: T): Future[string]{.async.} =
+  proc callMethod*[T](`method`: string, params: T): Future[string] {.async.} =
     let id = self.getId()
     self.incId()
     let jo = initGRequest(id = id, `method` = `method`, params = cast[JsonNode](params))
-    let str = $ % jo
+    let str = $ %jo
     result = await self.roundtrip(str)
